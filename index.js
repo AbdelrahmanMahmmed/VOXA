58f50174c24aec3c95691a68186500b645438f28
Abdelrahman Mahmmed <abdelrahmanmahmmed29@gmail.com>
2025-08-21 00:54:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-// const app = require("./app");
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, World!');
-// });
-
-// const PORT = process.env.PORT || 27017;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-
 const app = require("./app");
-const http = require("http");
-const { initSocket } = require("./interfaces/socket");
 
-const server = http.createServer(app);
-
-initSocket(server);
+app.get('/', (req, res) => {
+    res.send('Hello, VOXA!');
+});
 
 const PORT = process.env.PORT || 27017;
-server.listen(PORT, () => {
-    console.log(`🚀 Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
